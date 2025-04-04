0eeac51ba356eef6c488e0ce3a4fa27b8b20cee1
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-04-04 15:17:57 +0900

    Add the Discord related extended Robot class

---
+++ KamomeCAlgoExtensions/ExtendedRobot.cs
@@ -0,0 +1,41 @@
+﻿using cAlgo.API;
+using Discord.WebSocket;
+using KamomeCAlgoExtensions.Discord;
+
+namespace KamomeCAlgoExtensions;
+
+public abstract class ExtendedRobot : Robot, IDiscordProperty
+{
+    [Parameter("Discord API Token", Group = "Discord")]
+    public string DiscordApiToken { get; set; } = null!;
+
+    [Parameter("Discord Guild ID", Group = "Discord")]
+    public string DiscordGuildId { get; set; } = null!;
+
+    [Parameter("Discord Channel ID", Group = "Discord")]
+    public string DiscordChannelId { get; set; } = null!;
+
+    [Parameter("Discord Timeout Seconds for Ready", Group = "Discord", DefaultValue = 10)]
+    public int DiscordReadyTimeoutSecs { get; set; } = 10;
+
+    public DiscordSocketClient DiscordClient { get; private set; } = null!;
+    public SocketGuild DiscordGuild { get; private set; } = null!;
+    public SocketTextChannel DiscordChannel { get; private set; } = null!;
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        var task = SetDiscordPropertiesAsync();
+        task.Wait();
+    }
+
+    private async Task SetDiscordPropertiesAsync()
+    {
+        var timeout = TimeSpan.FromSeconds(DiscordReadyTimeoutSecs);
+        var (client, guild, channel) =
+            await DiscordCreationHelper.CreateAsync(DiscordApiToken, DiscordGuildId, DiscordChannelId, timeout);
+        DiscordClient = client;
+        DiscordGuild = guild;
+        DiscordChannel = channel;
+    }
+}
