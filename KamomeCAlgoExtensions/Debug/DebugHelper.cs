d5221811bc6add0772921e0b5af736abbef04ad6
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-04-04 15:28:37 +0900

    Add the helper to launch debugger

---
+++ KamomeCAlgoExtensions/Debug/DebugHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace KamomeCAlgoExtensions.Debug;
+
+public static class DebugHelper
+{
+    public static void LaunchDebugger(Action<string> logger, TimeSpan delay)
+    {
+#if DEBUG
+        logger("Running in Debug mode.");
+        var debuggerLaunched = Debugger.Launch();
+        if (debuggerLaunched is false)
+        {
+            logger("Failed to launch the debugger.");
+            return;
+        }
+
+        var delayTask = Task.Delay(delay);
+        delayTask.Wait();
+#else
+        logger("Running in Release mode.");
+#endif
+    }
+}
