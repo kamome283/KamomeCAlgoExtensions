247391ff37b351e621521d93df779d3ab89ef1a5
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-04-04 14:41:21 +0900

    Add the Discord related interface that will be required future Robot development

---
+++ KamomeCAlgoExtensions/Discord/IDiscordProperty.cs
@@ -0,0 +1,10 @@
+﻿using Discord.WebSocket;
+
+namespace KamomeCAlgoExtensions.Discord;
+
+public interface IDiscordProperty
+{
+    public DiscordSocketClient DiscordClient { get; }
+    public SocketGuild DiscordGuild { get; }
+    public SocketTextChannel DiscordChannel { get; }
+}
