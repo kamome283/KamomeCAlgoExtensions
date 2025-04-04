c784ae32c9006770d08a54eb6bf94dc6191b248d
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-04-04 20:41:13 +0900

    Change the field to property so that we can override in derived class

--- KamomeCAlgoExtensions/ExtendedRobot.cs
+++ KamomeCAlgoExtensions/ExtendedRobot.cs
@@ -7,8 +7,8 @@ namespace KamomeCAlgoExtensions;
 
 public abstract class ExtendedRobot : Robot, IDiscordProperty
 {
-    protected readonly int DebuggerDelaySecs = 10;
-    protected readonly bool LaunchDebugger = false;
+    protected int DebuggerDelaySecs => 10;
+    protected bool LaunchDebugger => false;
 
     [Parameter("Discord API Token", Group = "Discord")]
     public string DiscordApiToken { get; set; } = null!;
